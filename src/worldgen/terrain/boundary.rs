1db57e5d5f6c691534c3acc502ddb1f1cb70e516
fr33zing <fr33zing@freezing>
2025-01-19 14:41:06 -0800

    fix boundary enforcement

--- src/worldgen/terrain/boundary.rs
+++ src/worldgen/terrain/boundary.rs
@@ -1,5 +1,5 @@
 use avian3d::prelude::*;
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::hashbrown::HashSet};
 use bevy_tnua::TnuaToggle;
 
 use super::CHUNK_SIZE_F;
@@ -14,13 +14,11 @@ pub struct LoadingBoundary {
 
 impl LoadingBoundary {
     pub fn new(chunk_pos: IVec3) -> Self {
-        let center = chunk_pos.as_vec3() * CHUNK_SIZE_F;
-        let half = CHUNK_SIZE_F / 2.0;
-
+        let world_pos = chunk_pos.as_vec3() * CHUNK_SIZE_F;
         Self {
             aabb: ColliderAabb {
-                min: center - half,
-                max: center + half,
+                min: world_pos,
+                max: world_pos + CHUNK_SIZE_F,
             },
         }
     }
@@ -30,26 +28,38 @@ pub fn enforce_loading_chunk_boundaries(
     mut commands: Commands,
     spatial_query: SpatialQuery,
     boundaries: Query<&LoadingBoundary>,
-    intersecting: Query<Entity, With<IntersectsBoundary>>,
+    intersecting_prev: Query<Entity, With<IntersectsBoundary>>,
 ) {
-    intersecting.iter().for_each(|entity| {
-        let mut commands = commands.entity(entity);
-        commands.remove::<GravityScale>();
-        commands.remove::<TnuaToggle>();
-        commands.remove::<Sleeping>();
-        commands.remove::<IntersectsBoundary>();
-    });
+    let mut intersecting_curr = HashSet::<Entity>::new();
 
     boundaries.iter().for_each(|boundary| {
         spatial_query
             .aabb_intersections_with_aabb(boundary.aabb)
             .iter()
             .for_each(|entity| {
+                intersecting_curr.insert(*entity);
+
+                if intersecting_prev.contains(*entity) {
+                    return;
+                }
+
                 let mut commands = commands.entity(*entity);
                 commands.insert(GravityScale(0.0));
                 commands.insert(TnuaToggle::Disabled);
                 commands.insert(Sleeping);
                 commands.insert(IntersectsBoundary);
             });
+    });
+
+    intersecting_prev.iter().for_each(|entity| {
+        if intersecting_curr.contains(&entity) {
+            return;
+        }
+
+        let mut commands = commands.entity(entity);
+        commands.remove::<GravityScale>();
+        commands.remove::<TnuaToggle>();
+        commands.remove::<Sleeping>();
+        commands.remove::<IntersectsBoundary>();
     });
 }
